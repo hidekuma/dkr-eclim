bad6727b6d5a977a4d0d3f98e440e2e77f5cf6b6
hidekuma <d.hidekuma@gmail.com>
2019-12-04 13:46:56 +0900

    Updated port and entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sudo Xvfb :1 -screen 0 1024x768x24 &
+#sudo update-alternatives --install /usr/bin/java java /usr/lib/jvm/java-8-openjdk-amd64/jre/bin/java 9999
+#sudo Xvfb :1 -screen 0 1024x768x24 &
 DISPLAY=:1 /opt/eclipse/eclimd
